020cc0a23338a50f471e4928708e6afb2946d988
sasko87 <astevkovski87@gmail.com>
2025-11-09 09:43:25 +0100

    fix index

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ app.use("/profile", protectRoute, getProfile);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-  app.get("/:page", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
